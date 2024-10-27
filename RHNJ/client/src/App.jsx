3646c1fe04cb47b65eb9dd91ff9064d6edd3d2fd
Jessica Lafferty <jessica.a.lafferty@gmail.com>
2024-10-27 13:57:47 -0400

    added more pages

--- RHNJ/client/src/App.jsx
+++ RHNJ/client/src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; // pulled Switch out of import************
 import Login from "./components/Login";
 import Signup from "./components/SignUp";
-import AboutCharacters from "./pages/AboutCharacters";
+import AboutCharacters from "./components/AboutCharacters";
 import AdminHome from "./pages/administratorHome";
 import DMHome from "./pages/dmHome";
 import PlayerHome from "./pages/playerHome";
 import Navigations from "./components/Navigations";
 import Home from "./components/Home";
-
+import About from "./components/About";
+import HowToPlay from "./components/HowToPlay";
+import AdminSignUp from "./components/AdminSignUp";
+import DmSignUp from "./components/DmSignUp";
+import './App.css';
 
 
 function App() {   
@@ -19,6 +23,10 @@ function App() {
     <Routes>
         <Route path='/signup' element={<Signup/>} />
         <Route path='/login' element={<Login/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/how-to-play' element={<HowToPlay/>} />
+        <Route path='/admin-signup' element={<AdminSignUp/>} />
+        <Route path='/dm-signup' element={<DmSignUp/>} /> 
         <Route path='/about-characters' element={<AboutCharacters/>} />
         <Route path='/admin-home' element={<AdminHome/>} />
         <Route path='/dm-home' element={<DMHome/>} />
