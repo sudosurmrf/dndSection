25e1142e592f20e74ab9275ad17b480a55f9c1c2
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2024-11-04 01:21:12 -0800

    view character details working

--- RHNJ/client/src/components/CharacterBuilder.jsx
+++ RHNJ/client/src/components/CharacterBuilder.jsx
@@ -80,12 +80,34 @@ const CharacterBuilder = ({ onCharacterSelect }) => {
     <div>
       {/* Character Dropdown */}
       <label htmlFor='character-select'>Choose a Character:</label>
+      <h3>Your Characters</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Level</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {characters.map((character) => (
+            <tr key={character.id}>
+              <td>{character.name}</td>
+              <td>{character.level}</td>
+              <td>
+              <button onClick={() => handleDelete(character.id)}>Delete</button>
+              <button onClick={() => setSelectedCharacter(character)}>View Details</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <select
         id='character-select'
         value={selectedCharacterId}
         onChange={handleCharacterChange}
       >
-        <option value=''>-- Select a Character --</option>
+        <option value=''>-- Create a new character--</option>
 
         {characters.map((character) => (
           <option key={character.id} value={character.id}>
--- RHNJ/client/src/pages/playerHome.jsx
+++ RHNJ/client/src/pages/playerHome.jsx
@@ -82,33 +82,11 @@ const PlayerHome = () => {
       <h2>Player Homepage</h2>
       {error && <p style={{ color: 'red' }}>{error}</p>}
       <button onClick={toggleForm}>
-        {showForm ? 'Cancel' : 'Add Character'}
+        {showForm ? 'Cancel' : 'Choose your character'}
         </button>
       {showForm && (
         <CharacterBuilder onClose={toggleForm} characters={characters} onCharacterSelect={handleCharacterSelect} />
       )}
-      <h3>Your Characters</h3>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Level</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {characters.map((character) => (
-            <tr key={character.id}>
-              <td>{character.name}</td>
-              <td>{character.level}</td>
-              <td>
-              <button onClick={() => handleDelete(character.id)}>Delete</button>
-              <button onClick={() => setSelectedCharacter(character)}>View Details</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
     </div>
   );
 };
